bae8da0825f99da325d1d3a346146600b418eed2
candace <candace.marzec@gmail.com>
2019-01-17 12:48:05 -0800

    adds routes for individual products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,7 +3,16 @@ class Api::ProductsController < ApplicationController
 
   def all_products
     @products = Product.all
-    render 'allproducts.json.jbuilder'
+    render 'all_products.json.jbuilder'
   end
 
+  def first_product
+    @product = Product.first
+    render 'first_product.json.jbuilder'
+  end 
+  
+  def second_product
+    @product = Product.second
+    render 'second_product.json.jbuilder'
+  end   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # verb "/urls" => "namespace/controllers#action"
   namespace :api do 
     get "/all_products_url" => "products#all_products"
+    get "/first_product_url" => "products#first_product"
+    get "/second_product_url" => "products#second_product"
   end
 end
