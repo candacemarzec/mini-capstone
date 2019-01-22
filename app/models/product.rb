1fda51cb7f8f1488f8e409ef7c120770e288104e
candace <candace.marzec@gmail.com>
2019-01-22 12:45:17 -0800

    adds partial

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+
+  def is_discounted?#implied T or F
+    price <=10
+  end
+
+  def tax 
+    0.09 * price 
+  end  
+
+  def total
+    price + tax 
+  end
+
 end
